0533dbf02073b0dfa5eac24a9b3ada9c07311a99
Judah Van Zandt <judahvz@astro.ucla.edu>
2021-08-02 16:02:19 -0700

    Added if statement and restructured to allow for skipping of astrometry because some targets arent in the HG catalog.
    Former-commit-id: d2dcd1ab1fd25e7cfe911e5919111de52046cf97 [formerly e2d405a455f0b402908676eb2221193de5d75254] [formerly e2d405a455f0b402908676eb2221193de5d75254 [formerly 23e541cd5670ab6e8e96dc0d4994af64a27875a4]] [formerly a35802fcc4467b7f2bc560ea8e873a394194365a [formerly a35802fcc4467b7f2bc560ea8e873a394194365a [formerly f0649695e7e2e65c0e920ac5f42fa478364c3eb9]]]
    Former-commit-id: a35802fcc4467b7f2bc560ea8e873a394194365a
    Former-commit-id: 9dccc355512a1514a3acc421c329241bbcb4b31a [formerly 4f210aff06c49bd74ca8931578113d5afffc593d]
    Former-commit-id: 81da9d47cdfd7bb53b22aa546545470a47615d77

--- runner.py
+++ runner.py
@@ -4,6 +4,8 @@ import numpy as np
 from scipy.stats import loguniform, beta
 
 import radvel as rv
+import matplotlib.pyplot as plt
+import matplotlib.patches as ptch
 
 from trends import helper_functions_wrapper as hlpw
 
@@ -13,7 +15,8 @@ M_sun = 1.988409870698051e+33
 M_jup = 1.8981245973360504e+30
 pc_in_cm = 3.086e18
 
-# params_star = (m_star, distance(pc), gdot, gdot_err, gddot, gddot_err, rv_baseline(days), max_rv of residuals, rv_epoch, delta_mu, delta_mu_err)
+# params_star = (m_star, distance(pc), gdot, gdot_err, gddot, gddot_err, 
+#               rv_baseline(days), max_rv of residuals, rv_epoch, delta_mu, delta_mu_err)
 
 params_191939 = (0.807, 1.7989500299953727e+20, 0.114, 0.006, -6e-5, 1.9e-5, 
                 430.2527364352718, 40.0581900021484, 2458847.780463, 0.12767382507786398, 0.034199052901953214)
@@ -25,6 +28,10 @@ params_HIP97166 = (0.91, 68*pc_in_cm, 0.013, 0.03, -3e-6, 3.2e-5,
 # 12572 params. rv_baseline and max_rv are estimated for ease. gddot := 0, so borrow its error from gdot
 params_12572 = (0.91, 65.9*pc_in_cm, -0.0595, 0.0032, 0, 0.0032,
                 550, 30, 2458991.236308, 0.0748781, 0.045100458)
+                
+                
+params_test = (0.91, 65.9*pc_in_cm, -0.0595, 0.0032, 0, 0.0032,
+                550, 30, 2458991.236308, None, None)
                 
 # T001194 params. rv_baseline and max_rv are estimated for ease.
 # params_T001194 = (0.98, 150.3*pc_in_cm, )
@@ -41,7 +48,7 @@ m_lim = (1.5, 2e2)
 
 grid_num = 100
 
-num_points = int(1e7)
+num_points = int(1e6)
 
 t_num = 2
 tick_num = 6
@@ -55,12 +62,40 @@ a_list, m_list, per_list, e_list, i_list, om_list, E_anom_rv, T_anom_astro, a_in
 
 print('made arrays')
 
+fig, ax = plt.subplots(figsize=(12,12), dpi = 300)
+
+
+# Some targets aren't in the Hip/Gaia catalog, so we can't make the astrometry posterior.
+if delta_mu == None or delta_mu_err == None:
+    astro_list = np.ones(num_points)
+    post_astro = np.ones((grid_num, grid_num))
+    print('all_ones')
+    
+else:
+    astro_list = hlpw.astro_post(delta_mu, delta_mu_err, m_star, d_star, a_list,
+                                 m_list, per_list, e_list, i_list, om_list,
+                                 T_anom_astro, num_points, grid_num, t_num)                      
+    post_astro = np.array(hlpw.prob_array(astro_list, a_inds, m_inds, grid_num))
+    post_astro = post_astro/post_astro.sum()
+    
+    t_contours_astro = hlpw.contour_levels(post_astro, [1,2])
+    post_astro_cont = ax.contourf(post_astro, t_contours_astro, cmap='Blues', extend='max', alpha=0.5)
+    
+
 rv_list = hlpw.rv_post(gammadot, gammadot_err, gammaddot, gammaddot_err, m_star, 
                         a_list, m_list, per_list, e_list, i_list, om_list, E_anom_rv, 
                         num_points, grid_num)
-astro_list = hlpw.astro_post(delta_mu, delta_mu_err, m_star, d_star, a_list,
-                             m_list, per_list, e_list, i_list, om_list,
-                             T_anom_astro, num_points, grid_num, t_num)
+post_rv = np.array(hlpw.prob_array(rv_list, a_inds, m_inds, grid_num))
+post_tot = np.array(hlpw.post_tot(rv_list, astro_list, grid_num, a_inds, m_inds))
+
+post_rv = post_rv/post_rv.sum()
+post_tot = post_tot/post_tot.sum()
+
+t_contours_rv = hlpw.contour_levels(post_rv, [1,2])
+t_contours_tot = hlpw.contour_levels(post_tot, [1,2])
+
+post_rv_cont = ax.contourf(post_rv, t_contours_rv, cmap='Greens', extend='max', alpha=0.5)
+post_tot_cont = ax.contourf(post_tot, t_contours_tot, cmap='Reds', extend='max', alpha=0.75)
 
 # np.save('erik_samples/rv_probs.npy', rv_list)
 # np.save('erik_samples/astro_probs.npy', astro_list)
@@ -69,21 +104,7 @@ astro_list = hlpw.astro_post(delta_mu, delta_mu_err, m_star, d_star, a_list,
 #
 # rv_list = np.load('erik_samples/rv_probs.npy')
 # astro_list = np.load('erik_samples/astro_probs.npy')
-                                                               
-post_rv = np.array(hlpw.prob_array(rv_list, a_inds, m_inds, grid_num))
-post_astro = np.array(hlpw.prob_array(astro_list, a_inds, m_inds, grid_num))
-post_tot = np.array(hlpw.post_tot(rv_list, astro_list, grid_num, a_inds, m_inds))
 
-
-post_rv = post_rv/post_rv.sum()
-post_astro = post_astro/post_astro.sum()
-post_tot = post_tot/post_tot.sum()
-
-
-
-import matplotlib.pyplot as plt
-import matplotlib.patches as ptch
-#
 # plt.imsave('post_rv.png', post_rv, origin='lower')
 # plt.imsave('post_astro.png', post_astro, origin='lower')
 # plt.imsave('post_tot.png', post_tot, origin='lower')
@@ -106,24 +127,14 @@ m_list = np.logspace(np.log10(m_lim[0]), np.log10(m_lim[1]), grid_num)
 min_m = rv.utils.Msini(max_rv, min_per, m_star, e=0, Msini_units='jupiter')
 min_a = rv.utils.semi_major_axis(min_per, (m_star + min_m*(M_jup/M_sun)))
 
-
 min_index_m = hlpw.value2index(min_m, (0, grid_num-1), m_lim)
 min_index_a = hlpw.value2index(min_a, (0, grid_num-1), a_lim)
 
 
-t_contours_rv = hlpw.contour_levels(post_rv, [1,2])
-t_contours_astro = hlpw.contour_levels(post_astro, [1,2])
-t_contours_tot = hlpw.contour_levels(post_tot, [1,2])
-
+# Print out the 2-sigma boundaries for the total posterior
 bounds = hlpw.bounds_1D(post_tot, [m_lim, a_lim], interp_num = 1e4)
 print('a_lim, m_lim = ', bounds[0], bounds[1])
 
-
-fig, ax = plt.subplots(figsize=(12,12), dpi = 300)
-
-post_astro_cont = ax.contourf(post_astro, t_contours_astro, cmap='Blues', extend='max', alpha=0.5)
-post_rv_cont = ax.contourf(post_rv, t_contours_rv, cmap='Greens', extend='max', alpha=0.5)
-post_tot_cont = ax.contourf(post_tot, t_contours_tot, cmap='Reds', extend='max', alpha=0.75)
 
 mass_rect = ptch.Rectangle((0, 0), grid_num-1, min_index_m, color='gray', alpha=1.0)
 a_rect = ptch.Rectangle((0, 0), min_index_a, grid_num-1, color='gray', alpha=1.0)
@@ -145,7 +156,6 @@ plt.text((1/3)*(min_index_a-1), (1/8)*grid_num, 'Ruled out by minimum period', s
 
 ax.set_xlabel('Semi-major Axis (au)', size=label_size)
 ax.set_ylabel(r'$M_p$ ($M_{Jup}$)', size=label_size)
-# ax.set_title('Astrometry', size=title_size)
 ###################################################
 
 tick_array = np.linspace(0, grid_num-1, tick_num).astype(int)
