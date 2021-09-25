5871dd17363d9f86a27d19dda7518a05dd19b032
Judah Van Zandt <judahvz@astro.ucla.edu>
2021-09-25 11:00:14 -0700

    Small plotting changes

--- runner.py
+++ runner.py
@@ -209,7 +209,7 @@ if __name__ == "__main__":
     
     
     m_star, post_tot, post_rv, post_astro, grid_num, a_lim, m_lim, (min_a, min_m) = \
-            run(read_file=None, save=True, write_file='test_post', num_points=1e8, grid_num=100)
+            run(read_file=None, save=True, write_file='test_post', num_points=1e6, grid_num=100)
 
 
     plotter.joint_plot(m_star, post_tot, post_rv, post_astro, grid_num, a_lim, m_lim, (min_a, min_m), 
