Fixing an indexing problem in CDF bounds calculation. Not a big change for large grid_num, but now corrected. Also, last commit with commented-out PDF code.

plotter.py
@@ -158,7 +158,7 @@ def joint_plot(star_name, m_star, post_tot, post_rv, post_astro, grid_num, a_lim
     # twosig_inds contains the indices where the above horizontal line crosses the posterior. In case it crosses more than twice, it contains the first and last instances.
     bounds, twosig_inds = hlp.bounds_1D(post_tot, [m_lim, a_lim], 2, interp_num = 1e4)
     
-    print(twosig_inds)
+    print('HERE THEY ARE', twosig_inds)
     if marginalized:
         
         title_size = 30
@@ -170,23 +170,25 @@ def joint_plot(star_name, m_star, post_tot, post_rv, post_astro, grid_num, a_lim
         sma_1d = post_tot.sum(axis=0)
         mass_1d = post_tot.sum(axis=1)
 
-        ax[0].plot(range(grid_num), np.cumsum(sma_1d))
+        ax[0].plot(range(grid_num+1), np.insert(np.cumsum(sma_1d), 0, 0))
         plt.sca(ax[0])
         plt.xticks(tick_positions_a, tick_labels_a, size=tick_size)
         plt.yticks(size=tick_size)
         plt.title('Semi-major axis CDF', size=title_size)
         plt.xlabel('Companion semi-major axis (AU)', size = label_size)
-        #ax[0].hlines(twosig_levels[0], 0, grid_num-1, colors='k', linestyles='solid') # Irrelevant now with CDF
+
+        ax[0].hlines(0, 0, grid_num-1, colors='k', linestyles='solid')
         ax[0].vlines(twosig_inds[0][0], 0, 1, colors='r', linestyles='dashed')
         ax[0].vlines(twosig_inds[0][1], 0, 1, colors='r', linestyles='dashed')
 
-        ax[1].plot(range(grid_num), np.cumsum(mass_1d))
+        ax[1].plot(range(grid_num+1), np.insert(np.cumsum(mass_1d), 0, 0))
         plt.sca(ax[1])
         plt.xticks(tick_positions_m, tick_labels_m, size=tick_size)
         plt.yticks(size=tick_size)
         plt.xlabel(r'Companion mass ($M_{Jup}$)', size = label_size)
         plt.title('Mass CDF', size=title_size)
-        #ax[1].hlines(twosig_levels[1], 0, grid_num-1, colors='k', linestyles='solid')
+
+        ax[1].hlines(0, 0, grid_num-1, colors='k', linestyles='solid')
         ax[1].vlines(twosig_inds[1][0], 0, 1, colors='r', linestyles='dashed')
         ax[1].vlines(twosig_inds[1][1], 0, 1, colors='r', linestyles='dashed')
         