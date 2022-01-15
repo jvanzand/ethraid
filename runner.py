f2d916d246c8a382263fc06a19d298d6ae3a6806
Judah Van Zandt <judahvz@astro.ucla.edu>
2022-01-15 12:01:40 -0800

    Last commit before trying padding change.

--- runner.py
+++ runner.py
@@ -184,12 +184,31 @@ def run(star_name, m_star, d_star, gammadot, gammadot_err, gammaddot, gammaddot_
                 a_lim, m_lim, (min_a, min_m), period_lines = False)
     
     return
-
+    
+    
+    
+    
+    
+    
+    
 
 if __name__ == "__main__":
     
-    run(*sp.params_12572, num_points=1e7, grid_num=100, plot=True, read_file=None)
+    run(*sp.params_12572, num_points=1e6, grid_num=100, plot=True, read_file=None)
     # run(*sp.params_synth, num_points=1e6, grid_num=100, save=False, plot=True)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
