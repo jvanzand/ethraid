061bba5adfc0707038508593fc02bb334146c587
Judah Van Zandt <judahvz@astro.ucla.edu>
2022-02-23 14:13:22 -0800

    Small changes

--- trends/helper_functions_general.pyx
+++ trends/helper_functions_general.pyx
@@ -81,6 +81,7 @@ def make_arrays(double m_star, tuple a_lim, tuple m_lim, int grid_num, int num_p
     per_list = P_list(a_list, m_list, m_star) # Use this line when we are sampling a_tot, not a_planet
     
     # Eccentricities drawn from a beta distribution. In this branch I'm trying e distributions from Bowler 2020 to better represent the planet masses/separations I'm sampling.
+    #e_list = np.zeros(num_points)
     #e_list = spst.beta(0.95, 1.3).rvs(num_points)
     #e_list = np.where(e_list > 0.99, 0.99, e_list) # Replace e > 0.99 with 0.99
     
