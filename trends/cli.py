019e390699d6788e4dfdc57c7103a3647deb3bb8
Judah Van Zandt <judahvz@astro.ucla.edu>
2023-01-18 09:52:16 -0800

    Small cli changes

--- trends/cli.py
+++ trends/cli.py
@@ -9,6 +9,7 @@ from runner import run
 import trends.system_params as sp
 ###############
 
+# def main():
 parser = ArgumentParser(description="Perform trend analysis")
 
 parser.add_argument('-sn', '--star_name', type=str, metavar='\b', required=True,
@@ -17,7 +18,7 @@ parser.add_argument('-ms', '--m_star', type=float, metavar='\b', required=True,
                     help='Stellar mass in units of Jupiter masses')
 parser.add_argument('-ds', '--d_star', type=float, metavar='\b', required=True,
                     help='Distance to the host star in AU')
-                    
+                
 parser.add_argument('-gd', '--gdot', type=float, metavar='\b', required=True,
                     help='Linear trend in RVs in m/s/day')
 parser.add_argument('-gde', '--gdot_err', type=float, metavar='\b', required=True,
@@ -27,7 +28,7 @@ parser.add_argument('-bl', '--baseline', type=float, metavar='\b', required=True
                     help='Length of RV time baseline in days')
 parser.add_argument('-rvep', '--rv_epoch', type=float, metavar='\b', required=True,
                     help='Epoch of RV timeseries in BJD, usually around baseline midpoint')
-                    
+                
 # required=False so we can have targets with no curvature
 # default=0/1e8 so that if the flag isn't passed, we have values to pass to RV code
 # nargs='?' so I can pass a blank argument into Cadence (useful for running lists of targets)
@@ -38,7 +39,7 @@ parser.add_argument('-gdd', '--gddot', type=float, metavar='\b',
 parser.add_argument('-gdde', '--gddot_err', type=float, metavar='\b', 
                     required=False, default=1e8, nargs='?', const=1e8,
                     help='Error on gamma_ddot')
-                    
+                
 # required=False for targets with no astrometry
 # nargs='?' so I can pass blank args for Cadence (because in a list of stars, some don't have dmu and some do; my .sh script has to provide a single command with which all stars are run)
 # default=None and const=None by default, no need to set
@@ -62,7 +63,7 @@ parser.add_argument('-cs', '--contrast_str', type=str, metavar='',
                     required=False, nargs='?',
                     help='Path to dataframe containing contrast curve')
 
-# nargs='+' so I can pass both semi-major axis and mass with the same flag
+# nargs='+' so I can use the same flag to pass both semi-major axis and mass of a companion to plot
 parser.add_argument('-sp', '--scatter_plot', type=float, metavar='\b', required=False, nargs='+',
                     help='Semi-major axis (AU) and mass (M_J) of a known companion to plot. Separate values by a space only.')               
 parser.add_argument('-n', '--num_points', type=int, metavar='\b', required=False,
@@ -78,14 +79,16 @@ parser.add_argument('-p', '--plot', action='store_true', required=False,
 parser.add_argument('-r', '--read', type=str, metavar='\b', required=False,
                     help='File path to read in already-calculated posterior array')
 
+# parser.set_defaults(func=run)
 args = parser.parse_args()
+# args.func(args)
 
 if __name__=="__main__":
-    
-    run(args.star_name, args.m_star, args.d_star, args.gdot, args.gdot_err, 
-        args.gddot, args.gddot_err, args.baseline, 
+    # main()
+    run(args.star_name, args.m_star, args.d_star, args.gdot, args.gdot_err,
+        args.gddot, args.gddot_err, args.baseline,
         args.rv_epoch, args.delta_mu, args.delta_mu_err,
         vmag=args.vmag, imag_wavelength=args.imag_wavelength, contrast_str=args.contrast_str,
         scatter_plot=args.scatter_plot,
-        num_points=args.num_points, grid_num=args.grid_num, 
+        num_points=args.num_points, grid_num=args.grid_num,
         save=args.save, plot=args.plot, read_file_path=args.read)
