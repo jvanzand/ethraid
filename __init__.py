d5ec813901ad4830539b591c168e2e00731668cc
Judah Van Zandt <judahvz@astro.ucla.edu>
2023-02-10 14:11:11 -0800

    Some files deleted

--- __init__.py
+++
@@ -1,4 +0,0 @@
-print('ENTERED INIT')
-# from .plotter import *
-
-__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from setuptools import setup
-from Cython.Build import cythonize
-from distutils.extension import Extension
-import numpy as np
+from setuptools import setup, find_packages, Extension
+# from Cython.Build import cythonize
+from setuptools.command.build_ext import build_ext as _build_ext
+# from distutils.extension import Extension
+# import numpy as np
 import re
 
-import line_profiler
-profile = line_profiler.LineProfiler()
+import os
+import sys
+print('FILE PATH', os.path.abspath(os.path.dirname(__file__)))
+# import line_profiler
+# profile = line_profiler.LineProfiler()
 
 ####################
 # # Extra stuff to profile code using kernprof -l -v file.py after it is compiled
@@ -23,42 +27,71 @@ profile = line_profiler.LineProfiler()
 
 # First path is where to put compiled files (.so).
 # Second is where to find .pyx files
+## include_dirs=[np.get_include()], define_macros=[('CYTHON_TRACE', '1')]
 extensions = [
-    Extension("c_kepler._kepler",
-             ['c_kepler/_kepler.pyx'],
-              include_dirs=[np.get_include()], define_macros=[('CYTHON_TRACE', '1')]),
-    Extension("trends.compiled.helper_functions_general",
-             ['trends/helper_functions_general.pyx'],
-              include_dirs=[np.get_include()], define_macros=[('CYTHON_TRACE', '1')]),
-    Extension("trends.compiled.helper_functions_rv",
-             ['trends/helper_functions_rv.pyx'],
-              include_dirs=[np.get_include()], define_macros=[('CYTHON_TRACE', '1')]),
-    Extension("trends.compiled.helper_functions_astro",
-             ['trends/helper_functions_astro.pyx'],
-              include_dirs=[np.get_include()], define_macros=[('CYTHON_TRACE', '1')])
+    Extension("ethraid.compiled._kepler",
+             ['ethraid/c_kepler/_kepler.pyx']),
+    Extension("ethraid.compiled.helper_functions_general",
+             ['ethraid/helper_functions_general.pyx']),
+    Extension("ethraid.compiled.helper_functions_rv",
+             ['ethraid/helper_functions_rv.pyx']),
+    Extension("ethraid.compiled.helper_functions_astro",
+             ['ethraid/helper_functions_astro.pyx'])
 ]
 
 ##############################
 
-# # Specify a build directory to store the .c and .html files separately. Improves readability.
-# setup(
-#     ext_modules = cythonize(extensions, build_dir = 'trends/compiled', language_level='3', annotate=True)
-# )
+# From radvel but also found on StackExchange
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+# requirement_path = lib_folder + '/requirements.txt'
+def get_requires():
+    reqs = []
+    # print('WE MADE IT HEREEEEE')
+    # print('\n'*5)
+    # for line in open('ethraid/__init__.py').readlines():
+    #     print(sys.path)
+    #     print(line)
+    for line in open('requirements.txt', 'r').readlines():
+        reqs.append(line)
+    return reqs
+
+def get_property(prop, project):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
+                       open(project + '/__init__.py').read())
+    return result.group(1)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name = "trends",
-    packages = ["trends"],
+    name = "ethraid",
+    packages = find_packages(),
+    include_package_data = True,
+    package_data={'ethraid':['/data']},
+    setup_requires=['numpy'],
+    install_requires=get_requires(),
+    cmdclass={'build_ext':build_ext},
     entry_points = {
-        "console_scripts": ['trends = trends.cli:main']
+        "console_scripts": ['ethraid=ethraid.cli:main']
         },
-    version = "0.1.0",
+    version = get_property('__version__', 'ethraid'),
     description = "Command line application for trend code",
+    long_description = long_description,
     author = "Judah Van Zandt",
     author_email = "judahvz@astro.ucla.edu",
     url = "",
-    ext_modules = cythonize(extensions, build_dir = 'trends/compiled', language_level='3', annotate=True)
+    ext_modules = extensions
     )
-    
+
+# ext_modules = cythonize(extensions, build_dir = 'ethraid/compiled', language_level='3', annotate=True)
     
     
     
