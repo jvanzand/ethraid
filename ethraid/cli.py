0e45f7316e795fbbb6e1b1608a36d8a6aa2bc6be
Judah Van Zandt <judahvz@astro.ucla.edu>
2023-04-18 12:29:48 -0700

    Removed old arguments. Unneeded now that config files store most parameters.

--- ethraid/cli.py
+++ ethraid/cli.py
@@ -5,9 +5,6 @@ Command Line Interface
 from argparse import ArgumentParser
 import ethraid.driver
 
-# ###############
-# import ethraid.system_params as sp
-# ###############
 
 def main():
     psr = ArgumentParser(
@@ -32,11 +29,6 @@ def main():
                             default='',
                             help='Path to directory where output files will be saved'
                             )
-    # psr_parent.add_argument('-cf', '--config',
-    #                         type=str,
-    #                         required=True,
-    #                         help='Relative path of configuration file.'
-    #                         )
     psr_parent.add_argument('-v', '--verbose', 
                             action='store_true', 
                             required=False,
@@ -44,8 +36,8 @@ def main():
                             )
 
     
-    # ## Run trend simulation
-    # #######################
+    ## Run trend simulation
+    #######################
     psr_run = subpsr.add_parser('run', parents=[psr_parent],
                                 description="Derive a-m posterior by forward modelling orbits",
                                 prefix_chars="-"
@@ -55,122 +47,12 @@ def main():
                          required=True,
                          help='Relative path of configuration file.'
                          )
-    #
-    # psr_run.add_argument('-sn', '--star_name',
-    #                     type=str,
-    #                     required=True,
-    #                     help='Name of host star for file naming. Need not be official.')
-    # psr_run.add_argument('-ms', '--m_star',
-    #                     type=float,
-    #                     required=True,
-    #                     help='Stellar mass in units of Jupiter masses')
-    # psr_run.add_argument('-ds', '--d_star',
-    #                     type=float,
-    #                     required=True,
-    #                     help='Distance from Earth to the host star in AU')
-    #
-    # psr_run.add_argument('-gd', '--gammadot',
-    #                     type=float,
-    #                     required=True,
-    #                     help='Linear trend in RVs in m/s/day')
-    # psr_run.add_argument('-gde', '--gammadot_err',
-    #                     type=float,
-    #                     required=True,
-    #                     help='Error on gamma_dot')
-    #
-    # # required=False so we can have targets with no curvature
-    # # default=0/1e8 so that if the flag isn't provided, we have values to pass to RV code
-    # # nargs='?' so I can pass a blank argument into Cadence (useful for running lists of targets)
-    # # const=0/1e8 so that if the flag IS passed but with NO args (like on Cadence), default values are filled in
-    # psr_run.add_argument('-gdd', '--gammaddot',
-    #                     type=float,
-    #                     required=False,
-    #                     default=0,
-    #                     nargs='?',
-    #                     const=0,
-    #                     help='Curvature in RVs in m/s/day/day')
-    # psr_run.add_argument('-gdde', '--gammaddot_err',
-    #                     type=float,
-    #                     required=False,
-    #                     default=1e8,
-    #                     nargs='?',
-    #                     const=1e8,
-    #                     help='Error on gamma_ddot')
-    #
-    # psr_run.add_argument('-rvb', '--rv_baseline',
-    #                     type=float,
-    #                     required=True,
-    #                     help='Length of RV time baseline in days')
-    # psr_run.add_argument('-rvep', '--rv_epoch',
-    #                     type=float,
-    #                     required=True,
-    #                     help='Epoch of RV timeseries in BJD, usually around baseline midpoint')
-    #
-    # # required=False for targets with no astrometry
-    # # nargs='?' so I can pass blank args for Cadence (because in a list of stars, some don't have dmu and some do; my .sh script has to provide a single command with which all stars are run)
-    # # default=None and const=None by default, no need to set
-    # psr_run.add_argument('-dmu', '--delta_mu',
-    #                     type=float,
-    #                     required=False,
-    #                     nargs='?',
-    #                     help='Change in astrometric proper motion in milliarcseconds/yr')
-    # psr_run.add_argument('-dmue', '--delta_mu_err',
-    #                     type=float,
-    #                     required=False,
-    #                     nargs='?',
-    #                     help='Error on dmu')
-    # psr_run.add_argument('-hid', '--hip_id',
-    #                     type=str,
-    #                     required=False,
-    #                     help='Hipparcos identifier of stellar host')
-    # psr_run.add_argument('-gid', '--gaia_id',
-    #                     type=str,
-    #                     required=False,
-    #                     help='Gaia DR3 identifier of stellar host')
-    #
-    # # required=False for targets without imaging
-    # # nargs='?' to let me pass the flag with no args for Cadence
-    # # default=None and const=None by default, no need to set
-    # psr_run.add_argument('-vmag', '--vmag',
-    #                     type=float,
-    #                     required=False,
-    #                     nargs='?',
-    #                     help='Apparent V-band magnitude of host star')
-    # psr_run.add_argument('-imwav', '--imag_wavelength',
-    #                     type=float,
-    #                     required=False,
-    #                     nargs='?',
-    #                     help='Wavelength of imaging observations (μm)')
-    # psr_run.add_argument('-cs', '--contrast_str',
-    #                     type=str,
-    #                     required=False,
-    #                     nargs='?',
-    #                     help='Path to dataframe containing contrast curve')
-    # psr_run.add_argument('-n', '--num_points',
-    #                     type=int,
-    #                     required=False,
-    #                     default=1000000,
-    #                     help='Number of orbit models to run')
-    # run function needs grid_num argument, so default=100
     psr_run.add_argument('-gn', '--grid_num',
                         type=int,
                         required=False,
                         default=100,
                         help='Dimension of binned probability arrays')
-    
-    # # # The 'save' arg is special. Rather than having an assigned type, the "action='store_true'" argument assumes 1) boolean type and 2) default = False if the flag is not given. It also allows there to be no argument after the flag (in which case the value is set to True).
-    # psr_run.add_argument('-s', '--save',
-    #                     nargs='+',
-    #                     default='proc',
-    #                     choices=['raw', 'proc'],
-    #                     required=False,
-    #                     help = 'How to save probability arrays. \n'
-    #                            'Pass no flag to save processed arrays. \n'
-    #                            'Pass with "raw" or "proc" flags to save raw, processed, or both.'
-    #                     )
-    #
-    #
-    #
+
     psr_run.set_defaults(func=ethraid.driver.run)
     
     
@@ -203,13 +85,6 @@ def main():
                           default=None,
                           help='Dimension of binned probability array. Required for raw  input arrays.'
                           )
-    # nargs='+' so I can use the same flag to pass both semi_major axis and mass of a companion to plot
-    # psr_plot.add_argument('-sp', '--scatter_plot',
-    #                     type=float,
-    #                     required=False,
-    #                     nargs='+',
-    #                     help='Semi_major axis (AU) and mass (M_J) of a known companion for 2d plot.\
-    #                     Separate values by a space only.')
     psr_plot.set_defaults(func=ethraid.driver.plot)
     
     
