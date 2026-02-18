09d89472236e6dd5cb2790cd3aa1e055a8fe2e83
Judah Van Zandt <judahvz@astro.ucla.edu>
2026-02-18 10:51:49 -0800

    Changed version number for 3.2.2: improvements to 1D plots

--- ethraid/__init__.py
+++ ethraid/__init__.py
@@ -1,7 +1,7 @@
 import os
 from astropy.time import Time
 
-__version__='3.2.1' # Remember to remove random seed
+__version__='3.2.2' # Remember to remove random seed
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
